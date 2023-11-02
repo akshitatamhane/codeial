ba5e1abfe66156684df40c69a37bcded9e9f2b99
akshitatamhane <akshitatamhane93@gmail.com>
2023-11-02 18:25:19 +0530

    added files for partials and layouts

---
+++ controllers/users_controller.js
@@ -0,0 +1,7 @@
+module.exports.profile=function(req,res){
+    res.render('user_profile',{
+        title:"User Profile"
+    })
+}
+
+
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express= require('express');
 const app=express();
 const port=8000;
+const expressLayouts=require('express-ejs-layouts'); 
+
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
 
 // use express router
 app.use('/',require('./routes/index'));
 //fetches index by default so we can do just route
+
+
+//setting up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port, function(err){
     if(err)
